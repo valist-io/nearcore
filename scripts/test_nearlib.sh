b9a63169388c4d0a0f88356f51060ba6cd038d58
Piotr Mikulski <piotr@near.org>
2021-12-12 19:15:25 -0800

    Make `test_nearlib.sh` work from any directory

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -1,30 +1,32 @@
 #!/usr/bin/env bash
 set -ex
 
-export RUST_BACKTRACE=full
-./scripts/start_unittest.py --local &
+repo_dir=$(realpath -e "$(dirname "${BASH_SOURCE[0]}")/..")
+
+# Builds and starts `neard`, assumes it's running in ${repo_dir}'
+# May fail silently, leading to hard to debug CI failures
+# TODO: add option to build only and run checks, this will allow us to see those errors synchronously.
+(cd "${repo_dir}"; RUST_BACKTRACE=full "${repo_dir}"/scripts/start_unittest.py --local) &
 export NEAR_PID=$!
 trap 'pkill -15 -P $NEAR_PID' 0
 
-#./scripts/build_wasm.sh
+# "${repo_dir}"/scripts/build_wasm.sh
 
-rm -rf near-api-js near-shell create-near-app
-git clone https://github.com/near/near-api-js.git
-git clone https://github.com/near/near-shell.git
-git clone https://github.com/near/create-near-app.git
+rm -rf "${repo_dir}"/near-api-js "${repo_dir}"/near-shell "${repo_dir}"/create-near-app
+git clone https://github.com/near/near-api-js.git "${repo_dir}"/near-api-js
+git clone https://github.com/near/near-shell.git "${repo_dir}"/near-shell
+git clone https://github.com/near/create-near-app.git "${repo_dir}"/create-near-app
 
 # Make sure to use local nearcore for tests
 export NODE_ENV=local
-
-SRC_DIR=$(pwd)/$(dirname "${BASH_SOURCE[0]}")
-export HOME=$SRC_DIR/../testdir
+export HOME="{$repo_dir}"/testdir
 
 # Run near-api-js tests
-cd near-api-js
-yarn
-yarn build
-../scripts/waitonserver.sh
-# Disabling yarn test for now
+(cd "${repo_dir}"/near-api-js; yarn)
+(cd "${repo_dir}"/near-api-js; yarn build)
+"${repo_dir}"/scripts/waitonserver.sh
+
+# TODO(#5757) Disabling yarn test for now
 # yarn test
 # yarn doc
 
